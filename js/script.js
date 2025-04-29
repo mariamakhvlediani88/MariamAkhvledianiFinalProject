f3847580bc75d938308c9daad72b1ef5e35c4b40
mariamakhvlediani88 <m.akhvlediani8@gmail.com>
2025-04-29 14:11:35 +0400

    header

--- js/script.js
+++
@@ -1,5 +0,0 @@
-var toggleButton = document.querySelector(".toggle-menu");
-var navBar = document.querySelector(".nav-bar");
-toggleButton.addEventListener("click", function () {
-  navBar.classList.toggle("toggle");
-});
